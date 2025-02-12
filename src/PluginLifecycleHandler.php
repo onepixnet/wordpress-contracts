3935bda3cb062b1351465fa1f54b33ed85f56536
daniel21703 <daniel217032001@gmail.com>
2025-02-12 22:16:27 +0300

    Update PluginLifecycleHandler

--- src/PluginLifecycleHandler.php
+++ src/PluginLifecycleHandler.php
@@ -7,6 +7,8 @@ namespace OnePix\WordPressContracts;
 interface PluginLifecycleHandler
 {
     public function registerActivationHook(callable $callback): void;
+
     public function registerDeactivationHook(callable $callback): void;
+
     public function registerUninstallHook(callable $callback): void;
 }
