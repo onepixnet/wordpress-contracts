06cee8c9c9233ab616b70411e60b14e2333cd42f
daniel21703 <daniel217032001@gmail.com>
2025-02-12 23:25:23 +0300

    Update Actions interfaces

--- src/ActionsDispatcher.php
+++ src/ActionsDispatcher.php
@@ -6,5 +6,8 @@ namespace OnePix\WordPressContracts;
 
 interface ActionsDispatcher
 {
+    /**
+     * @param  non-empty-string  $hook
+     */
     public function do(string $hook,mixed ...$args): void;
 }
--- src/ActionsRegistrar.php
+++ src/ActionsRegistrar.php
@@ -6,11 +6,25 @@ namespace OnePix\WordPressContracts;
 
 interface ActionsRegistrar
 {
+    /**
+     * @param  non-empty-string  $hook
+     * @param  callable(mixed):void  $callback
+     */
     public function add(string $hook, callable $callback, int $priority = 10, int $acceptedArgs = 1): void;
 
+    /**
+     * @param  non-empty-string  $hook
+     * @param  callable(mixed):void  $callback
+     */
     public function remove(string $hook, callable $callback, int $priority = 10): void;
 
+    /**
+     * @param  non-empty-string  $hook
+     */
     public function has(string $hook, callable $callback): bool;
 
+    /**
+     * @param  non-empty-string  $hook
+     */
     public function hasAny(string $hook): bool;
 }
