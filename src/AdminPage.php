bec8026db308c839f4ea646a9ac056047e672f52
daniel21703 <daniel217032001@gmail.com>
2025-02-13 11:54:31 +0300

    Update AdminPage interface

--- src/AdminPage.php
+++ src/AdminPage.php
@@ -22,5 +22,18 @@ interface AdminPage
 
     public function getPosition(): ?int;
 
+    /**
+     * Simply use @see \OnePix\WordPressContractsImplementation\get_plugin_page_hookname
+     */
     public function getPageHookName(): string;
+
+    /**
+     * Enqueue scripts or just output it
+     */
+    public function enqueueScripts(): void;
+
+    /**
+     * Enqueue styles or just output it
+     */
+    public function enqueueStyles(): void;
 }
